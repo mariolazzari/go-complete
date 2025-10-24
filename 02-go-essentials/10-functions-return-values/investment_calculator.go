db66c8ebdab6ec54dcc971d456159ac20ccf2404
Mario Lazzari <mario.lazzari@gmail.com>
2025-10-24 08:07:29 +0200

    Done section 2 lesson 35

---
+++ 02-go-essentials/10-functions-return-values/investment_calculator.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const inflationRate = 2.5
+
+func main() {
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
+
+	outputText("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	outputText("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	outputText("Years: ")
+	fmt.Scan(&years)
+
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for Inflation): %.1f\n", futureRealValue)
+
+	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
+}
